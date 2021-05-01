60275ab33b0226440f6561c1ff309f4bb332b711
Alexi Vandevoorde <alexi@superprof.fr>
2021-05-01 21:30:54 +0200

    Added wansview ptz node file

---
+++ wansview_ptz.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const onvif = require('node-onvif');
+var myArgs = process.argv.slice(2);
+
+var user = (myArgs[0]);
+var pass = (myArgs[1]);
+var host = (myArgs[2]);
+var port = (myArgs[3]);
+var xx = parseFloat(myArgs[4]);
+var yy = parseFloat(myArgs[5]);
+
+let device = new onvif.OnvifDevice({
+	xaddr: 'http://' + host + ':' + port + '/onvif/device_service',
+	user : user,
+	pass : pass
+});
+
+device.init().then(() => {
+	let params = {
+	  'speed': {x: xx, y: yy, z: 0.0},
+	  'timeout': 60 // seconds
+	};
+	// Supposed to move the camera for 60 seconds
+	device.ptzMove(params).then(() => {
+	  console.log('Succeeded to move.');
+	  setTimeout(() => {
+	    device.ptzStop().then(() => {
+	      console.log('Succeeded to stop.');
+	    }).catch((error) => {
+	      console.error(error);
+	    });
+	  }, 500);
+	}).catch((error) => {
+	  console.error(error);
+	});
+});
